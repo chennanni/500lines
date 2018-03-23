34b6fc42423837144d2d37a0046b5bef6a60107b
Chennan <chennancd@gmail.com>
2018-03-23 14:10:05 +0800

    Update run_or_fail.sh

--- ci/code/run_or_fail.sh
+++ ci/code/run_or_fail.sh
@@ -1,8 +1,9 @@
 # helper method for providing error messages for a command
 run_or_fail() {
   local explanation=$1
-  shift 1
-  "$@"
+  shift 1 # removing the first parameter
+  "$@" # all remaining parameters, which means executing the git cmd in this case
+  # output error message
   if [ $? != 0 ]; then
     echo $explanation 1>&2
     exit 1
