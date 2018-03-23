f0db70583d1aef4507a4b46f168d7b86e1998d60
Chennan <chennancd@gmail.com>
2018-03-23 14:09:53 +0800

    Update test_runner_script.sh

--- ci/code/test_runner_script.sh
+++ ci/code/test_runner_script.sh
@@ -4,7 +4,9 @@ COMMIT=$2
 
 source run_or_fail.sh
 
-run_or_fail "Repository folder not found" pushd "$REPO" 1> /dev/null
+# updates the repository to the given commit ID
+
+run_or_fail "Repository folder not found" pushd "$REPO" 1> /dev/null # saves the current working directory in memory so it can be returned to at any time
 run_or_fail "Could not clean repository" git clean -d -f -x
 run_or_fail "Could not call git pull" git pull
 run_or_fail "Could not update to given commit hash" git reset --hard "$COMMIT"
